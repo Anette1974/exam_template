72d9a98fc22b7515eaaa4f7402aca0c85a829b3a
Anette1974 <anette.cedergren@yahoo.se>
2025-02-28 07:58:29 +0100

    Loopar nya innerväggar enligt två listor med x och y koordinator (små justeringar i def horiz_inner_walls)

--- src/walls.py
+++ src/walls.py
@@ -5,16 +5,13 @@ class Walls:
         self.horiz_inner_walls()
         self.g = grid
 
-
-
+    # skapar extra innerväggar i spelplanen
     def horiz_inner_walls(self):
         horizontal = [5, 6, 7, 15, 16, 17, 22, 22, 22]
         vertical = [3, 3, 3, 9, 9, 9, 4, 5, 6]
-        # skapar extra innerväggar i spelplanen
-        #self.g.set(10, 3, "■") # sätter ut en vägg på en fast position
         for x, y, in zip(horizontal, vertical):
                 self.g.set(x, y, "■")
 
+        #self.g.set(10, 3, "■") # första försöket, sätter ut en vägg på en fast position
 
-
-    g = Grid() # skapar en instans av klassen Grid
+    g = Grid()  # skapar en instans av klassen Grid
